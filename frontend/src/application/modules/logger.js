16d7c4883cbd2e1e3b4911ebe9b69ad4c70f99b6
GethsLeader <gethsleader@gmail.com>
2018-03-10 13:33:22 +0600

    [backend] Logger module improved.

--- frontend/src/application/modules/logger.js
+++ frontend/src/application/modules/logger.js
@@ -8,6 +8,7 @@ export class Logger {
     constructor(level) {
         this._levels = ['debug', 'info', 'log', 'warn', 'error'];
         this.level = level || 'log';
+        this._init();
         this.debug('[Logger] constructed.');
     }
 
@@ -24,34 +25,39 @@ export class Logger {
         return this._levels.indexOf(checkingLevel) >= this._levels.indexOf(this._level);
     }
 
-    debug() {
-        if (this._enabled && this._checkLevel('debug')) {
-            console.info.apply(console, arguments);
+
+    _init() {
+        if (this._enabled) {
+            if (this._checkLevel('debug')) {
+                this.debug = console.log.bind(console);
+            } else {
+                this.debug = this._doNotLog.bind(this);
+            }
+            if (this._checkLevel('info')) {
+                this.info = console.log.bind(console);
+            } else {
+                this.info = this._doNotLog.bind(this);
+            }
+            if (this._checkLevel('log')) {
+                this.log = console.log.bind(console);
+            } else {
+                this.log = this._doNotLog.bind(this);
+            }
+            if (this._checkLevel('warn')) {
+                this.warn = console.warn.bind(console);
+            } else {
+                this.warn = this._doNotLog.bind(this);
+            }
+            if (this._checkLevel('error')) {
+                this.error = console.error.bind(console);
+            } else {
+                this.error = this._doNotLog.bind(this);
+            }
         }
+
     }
 
-    info() {
-        if (this._enabled && this._checkLevel('info')) {
-            console.info.apply(console, arguments);
-        }
-    }
-
-    log() {
-        if (this._enabled && this._checkLevel('log')) {
-            console.log.apply(console, arguments);
-        }
-    }
-
-    warn() {
-        if (this._enabled && this._checkLevel('warn')) {
-            console.warn.apply(console, arguments);
-        }
-    }
-
-    error() {
-        if (this._enabled && this._checkLevel('error')) {
-            console.error.apply(console, arguments);
-        }
+    _doNotLog() {
     }
 }
 
